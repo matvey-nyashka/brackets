8e814dddb10b1779697efac09a1a8329d41e1589
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-03-13 17:31:22 +0300

    feat: changed check function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-  let bracketsClose = bracketsConfig.reduce((acc, val) => acc.concat(val), []).filter((item, index) => (index%2 == 1)).map((value) => value.trim());
-  let bracketsOpen = bracketsConfig.reduce((acc, val) => acc.concat(val), []).filter((item, index) => (index%2 == 0)).map((value) => value.trim());
+  let bracketsClose = bracketsConfig.reduce((acc, val) => acc.concat(val), []).filter((item, index) => (index%2 == 1));
+  let bracketsOpen = bracketsConfig.reduce((acc, val) => acc.concat(val), []).filter((item, index) => (index%2 == 0));
   let arr_str = str.split('');
   for (let i = 0; i < arr_str.length; i++){
       if (bracketsClose.includes(arr_str[i])){
