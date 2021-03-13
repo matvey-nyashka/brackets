f2daf8678d624daa757d479408b99cd40530c3f4
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-03-13 17:27:40 +0300

    feat: added check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsClose = bracketsConfig.reduce((acc, val) => acc.concat(val), []).filter((item, index) => (index%2 == 1)).map((value) => value.trim());
+  let bracketsOpen = bracketsConfig.reduce((acc, val) => acc.concat(val), []).filter((item, index) => (index%2 == 0)).map((value) => value.trim());
+  let arr_str = str.split('');
+  for (let i = 0; i < arr_str.length; i++){
+      if (bracketsClose.includes(arr_str[i])){
+          let j = bracketsClose.indexOf(arr_str[i]);
+          if (arr_str[i-1] == bracketsOpen[j]){
+              arr_str.splice(i-1, 2);
+              i -= 2;
+          }
+      }
+  }
+  return arr_str.length > 0 ? false : true;
 }
